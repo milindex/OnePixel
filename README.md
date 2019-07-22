bb0ca2865f426384f943c8d6c9c830da7d99751d
YOLO Magic <milind4j@gmail.com>
2019-07-23 01:43:59 +0530

    Updated to Version 2.1 Please See Changelog

---
+++ README.md
@@ -0,0 +1,9 @@
+# OnePixel
+Systemlessly Tweak Your MiA1 with Various Pixel Like Features
+
+
+## Incompatibilities
+* Any Magisk Module that changes Fonts
+* **Tested on Magisk v19.3 MiA1 Pie July 2019 Update**
+
+#### Thread: https://forum.xda-developers.com/apps/magisk/module-emoji-one-t3477911
---
+++ common/service.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
+# This will make your scripts compatible even if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+# This script will be executed in late_start service mode
+# More info in the main Magisk thread
+
+# Disable backlight keys
+# Disable charging light
+echo 0 > sys/devices/soc/leds-qpnp-10/leds/button-backlight/max_brightness
+echo 0 > sys/class/leds/red/max_brightness
+
